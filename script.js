9a242561e5edfb348396883e00ab38dfc865e959
gilbertjusmc <jgilbert1341@gmail.com>
2019-11-22 11:01:10 -0500

    added api to portfolio page. Almost working

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-/*
+/* //This was an assignment for VWC
 setTimeout (function(){
     alert ("Welcome, you have been on my page for >8 seconds.");}, 8500);
 */
+
+/*
+document.getElementById('api-btn').addEventListener('click', getDrink);
+
+function getDrink() {
+    let display = [];
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      .then(result => {return result.json()})
+      .then(result => console.log(result.drinks[0]))   
+  }
+  */
