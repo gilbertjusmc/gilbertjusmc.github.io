b12fcee5cc648b9699675ab64037599c1f4b3bf8
Jason Gilbert <51034552+gilbertjusmc@users.noreply.github.com>
2019-11-22 13:07:03 -0500

    added more function to getDrink

--- script.js
+++ script.js
@@ -3,13 +3,36 @@ setTimeout (function(){
     alert ("Welcome, you have been on my page for >8 seconds.");}, 8500);
 */
 
-/*
+// wait for button click to run function
 document.getElementById('api-btn').addEventListener('click', getDrink);
 
 function getDrink() {
-    let display = [];
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-      .then(result => {return result.json()})
-      .then(result => console.log(result.drinks[0]))   
-  }
-  */
+	fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+
+		.then(result => {
+			return result.json()
+		})
+
+		.then(result => {
+			// console.log(result.drinks[0])
+			const drink = result.drinks[0];
+			const keys = Object.keys(drink);
+			// console.log(keys)
+			const ingredients = keys.map(key => {
+				if (key.indexOf('strIngredient') === 0) {
+					return drink[key]
+				};
+				// console.log('key', key)
+			}).filter(value => {
+				return value
+			});
+			console.log('ingredients = ', ingredients)
+		})
+
+	const output = document.querySelector('#output');
+}
+
+//TODO append name and instructions
+//TODO create array of ingredients , measure loop through ingredients
+//TODO use forEach to display to site
+//TODO output.append(textNode ingredient and measure)
