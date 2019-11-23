5d2e46074144770777b5c20a571c7fefdc67b252
gilbertjusmc <jgilbert1341@gmail.com>
2019-11-23 08:37:39 -0500

    started measure output

--- script.js
+++ script.js
@@ -7,29 +7,45 @@ setTimeout (function(){
 document.getElementById('api-btn').addEventListener('click', getDrink);
 
 function getDrink() {
-	fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 
-		.then(result => {
-			return result.json()
-		})
+    .then(result => {
+      return result.json()
+    })
 
-		.then(result => {
-			// console.log(result.drinks[0])
-			const drink = result.drinks[0];
-			const keys = Object.keys(drink);
-			// console.log(keys)
-			const ingredients = keys.map(key => {
-				if (key.indexOf('strIngredient') === 0) {
-					return drink[key]
-				};
-				// console.log('key', key)
-			}).filter(value => {
-				return value
-			});
-			console.log('ingredients = ', ingredients)
-		})
+    .then(result => {
+      // console.log(result.drinks[0])
+      const drink = result.drinks[0];
+      const keys = Object.keys(drink);
+      // console.log(keys)
+      const ingredients = keys.map(key => {
+        if (key.indexOf('strIngredient') === 0) {
+          return drink[key]
+        };
+        // console.log('key', key)
+      }).filter(value => {
+        return value
+      });
+      console.log('ingredients = ', ingredients)
+    })
 
-	const output = document.querySelector('#output');
+    .then(result => {
+      // console.log(result.drinks[0])
+      const drink = result.drinks[0];
+      const keys = Object.keys(drink);
+      // console.log(keys)
+      const measure = keys.map(key => {
+        if (key.indexOf('strMeasure') === 0) {
+          return drink[key]
+        };
+        // console.log('key', key)
+      }).filter(value => {
+        return value
+      });
+      console.log('Measure = ', measure)
+    })
+
+  const output = document.querySelector('#output');
 }
 
 //TODO append name and instructions
