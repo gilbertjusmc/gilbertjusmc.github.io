e1cfcc731489db88677e017eceb604b6d6df9c85
Jason Gilbert <51034552+gilbertjusmc@users.noreply.github.com>
2019-11-25 12:43:51 -0500

    added more to API homework

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
-/* //This was an assignment for VWC
-setTimeout (function(){
-    alert ("Welcome, you have been on my page for >8 seconds.");}, 8500);
-*/
+const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+const displayDrinkName = document.getElementById("output-name");
+const displayDrinkIngredients = document.getElementById("output-ingredients");
+// const displayDrinkMeasure = document.getElementById("output-measure");
+const displayInstructions = document.getElementById("output-instruct");
 
 // wait for button click to run function
-document.getElementById('api-btn').addEventListener('click', getDrink);
+document.getElementById("api-btn").addEventListener("click", getDrink);
 
 function getDrink() {
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-
+  fetch(url)
     .then(result => {
-      return result.json()
+      return result.json();
     })
 
     .then(result => {
-      // console.log(result.drinks[0])
       const drink = result.drinks[0];
       const keys = Object.keys(drink);
       // console.log(keys)
-      const ingredients = keys.map(key => {
-        if (key.indexOf('strIngredient') === 0) {
-          return drink[key]
-        };
-        // console.log('key', key)
-      }).filter(value => {
-        return value
+      const ingredients = keys
+        .map(key => {
+          if (key.indexOf("strIngredient") === 0) {
+            return drink[key];
+          }
+          // console.log('key', key)
+        })
+        .filter(value => {
+          return value;
+        });
+
+      const measure = keys
+        .map(key => {
+          if (key.indexOf('strMeasure') === 0) {
+            return drink[key]
+          };
+          // console.log('key', key)
+        }).filter(value => {
+          return value
+        });
+      // console.log('Measure = ', measure)
+      // console.log('ingredients = ', ingredients)
+
+      //TODO use forEach to display to site
+      //TODO output.append(textNode ingredient and measure)
+
+      const header = document.createElement('h2');
+      const drinkName = document.createTextNode(drink.strDrink)
+      header.appendChild(drinkName);
+      displayDrinkName.appendChild(header)
+
+
+      ingredients.forEach((ingredient, index) => {
+        // console.log(ingredient, measure[index]);
+        const para = document.createElement("p")
+        const newIngredient = document.createTextNode(ingredient + ':' + measure[index])
+        para.appendChild(newIngredient)
+        displayDrinkIngredients.appendChild(para);
       });
-      console.log('ingredients = ', ingredients)
+
+
     })
 
-    .then(result => {
-      // console.log(result.drinks[0])
-      const drink = result.drinks[0];
-      const keys = Object.keys(drink);
-      // console.log(keys)
-      const measure = keys.map(key => {
-        if (key.indexOf('strMeasure') === 0) {
-          return drink[key]
-        };
-        // console.log('key', key)
-      }).filter(value => {
-        return value
-      });
-      console.log('Measure = ', measure)
-    })
+    // ingredient.append(document.createTextNode())
 
-  const output = document.querySelector('#output');
+
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 //TODO append name and instructions
 //TODO create array of ingredients , measure loop through ingredients
-//TODO use forEach to display to site
-//TODO output.append(textNode ingredient and measure)
