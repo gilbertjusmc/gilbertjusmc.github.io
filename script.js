c6dfc073c5311a43c04dbd67693f0df934d68669
Jason Gilbert <51034552+gilbertjusmc@users.noreply.github.com>
2019-11-26 11:45:41 -0500

    added reset function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const displayDrinkName = document.getElementById("output-name");
 const displayImage = document.getElementById("output-img");
 const displayDrinkIngredients = document.getElementById("output-ingredients");
 const displayInstructions = document.getElementById("output-instruct");
+const button = document.getElementById('api-btn');
 
 // wait for button click to run function
 document.getElementById("api-btn").addEventListener("click", getDrink);
@@ -36,18 +37,24 @@ function getDrink() {
         });
 
       const header = document.createElement('h2');
+      // this is to setup a rest
+      header.setAttribute('id', 'header');
       const drinkName = document.createTextNode(drink.strDrink);
       header.appendChild(drinkName);
       displayDrinkName.appendChild(header);
 
       ingredients.forEach((ingredient, index) => {
         const para = document.createElement("p");
+        // this is to setup a rest
+        para.setAttribute('id', 'ingredients');
         const newIngredient = document.createTextNode(ingredient + ': ' + measure[index]);
         para.appendChild(newIngredient);
         displayDrinkIngredients.appendChild(para);
       });
 
       const instructions = document.createElement('p');
+      // this is to setup a rest
+      instructions.setAttribute('id', 'instructions');
       const drinkInstructions = document.createTextNode(drink.strInstructions);
       instructions.appendChild(drinkInstructions);
       displayInstructions.appendChild(instructions);
@@ -57,3 +64,12 @@ function getDrink() {
       console.log(error);
     });
 }
+// window.onload = getDrink();
+button.addEventListener('click', (reset = () => {
+  let head = document.getElementById('header');
+  let ingred = document.getElementById('ingredients');
+  let instruct = document.getElementById('instructions');
+  head.parentNode.removeChild(head);
+  ingred.parentNode.removeChild(ingred);
+  instruct.parentNode.removeChild(instruct);
+}));
