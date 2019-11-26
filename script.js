dfd893d867aad52bd65e2c33390cc4c8862b24a2
gilbertjusmc <jgilbert1341@gmail.com>
2019-11-25 19:52:45 -0500

    mostly done with API homework

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 const displayDrinkName = document.getElementById("output-name");
+const displayImage = document.getElementById("output-img");
 const displayDrinkIngredients = document.getElementById("output-ingredients");
-// const displayDrinkMeasure = document.getElementById("output-measure");
 const displayInstructions = document.getElementById("output-instruct");
 
 // wait for button click to run function
@@ -16,13 +16,11 @@ function getDrink() {
     .then(result => {
       const drink = result.drinks[0];
       const keys = Object.keys(drink);
-      // console.log(keys)
       const ingredients = keys
         .map(key => {
           if (key.indexOf("strIngredient") === 0) {
             return drink[key];
           }
-          // console.log('key', key)
         })
         .filter(value => {
           return value;
@@ -33,40 +31,29 @@ function getDrink() {
           if (key.indexOf('strMeasure') === 0) {
             return drink[key]
           };
-          // console.log('key', key)
         }).filter(value => {
           return value
         });
-      // console.log('Measure = ', measure)
-      // console.log('ingredients = ', ingredients)
-
-      //TODO use forEach to display to site
-      //TODO output.append(textNode ingredient and measure)
 
       const header = document.createElement('h2');
-      const drinkName = document.createTextNode(drink.strDrink)
+      const drinkName = document.createTextNode(drink.strDrink);
       header.appendChild(drinkName);
-      displayDrinkName.appendChild(header)
-
+      displayDrinkName.appendChild(header);
 
       ingredients.forEach((ingredient, index) => {
-        // console.log(ingredient, measure[index]);
-        const para = document.createElement("p")
-        const newIngredient = document.createTextNode(ingredient + ':' + measure[index])
-        para.appendChild(newIngredient)
+        const para = document.createElement("p");
+        const newIngredient = document.createTextNode(ingredient + ': ' + measure[index]);
+        para.appendChild(newIngredient);
         displayDrinkIngredients.appendChild(para);
       });
 
-
+      const instructions = document.createElement('p');
+      const drinkInstructions = document.createTextNode(drink.strInstructions);
+      instructions.appendChild(drinkInstructions);
+      displayInstructions.appendChild(instructions);
     })
 
-    // ingredient.append(document.createTextNode())
-
-
-    .catch(err => {
-      console.log(err);
+    .catch(error => {
+      console.log(error);
     });
 }
-
-//TODO append name and instructions
-//TODO create array of ingredients , measure loop through ingredients
